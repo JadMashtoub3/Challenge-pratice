b71082ba19c37163b80a90c91abd1f31dd4713cf
Jad <103596586@student.swin.edu.au>
2022-06-25 16:55:18 +1000

    backend made

---
+++ Database/storedprocedures.sql
@@ -0,0 +1,62 @@
+--add treatment
+IF OBJECT_ID('ADD_TREATMENT') IS NOT NULL
+    DROP PROCEDURE ADD_TREATMENT;
+GO
+
+CREATE PROCEDURE ADD_TREATMENT @pownerID int, @ppetName NVARCHAR(100), @pprocedureID INT, @ptreatmentDate DATE, @pnotes NVARCHAR(100), @ppayment MONEY 
+AS
+BEGIN
+    BEGIN TRY
+        INSERT INTO TREATMENT(ownerID, petName, procedureID, treatmentDate, notes, payment)
+        VALUES(@pownerID, @ppetName, @pprocedureID, @ptreatmentDate, @pnotes, @ppayment);
+    END TRY
+
+    BEGIN CATCH
+        IF ERROR_NUMBER() = 2627
+            THROW 50020, 'unable to add Treatment', 1
+    END CATCH
+    END
+GO
+
+
+
+-- delete treatment
+IF OBJECT_ID('DELETE_TREATMENT') IS NOT NULL
+    DROP PROCEDURE DELETE_TREATMENT
+GO
+CREATE PROCEDURE DELETE_TREATMENT
+    @pownerID int,
+    @ppetName NVARCHAR(100),
+    @pprocedureID INT 
+AS
+BEGIN
+    BEGIN TRY
+    DELETE FROM TREATMENT
+    WHERE ownerID = @pownerID
+        AND petName = @ppetName
+        AND procedureID = @pprocedureID
+    END TRY
+    BEGIN CATCH
+    END CATCH
+END
+GO
+
+--delete owner
+
+IF OBJECT_ID('DELETE_OWNER') IS NOT NULL
+    DROP PROCEDURE DELETE_OWNER
+GO
+CREATE PROCEDURE DELETE_OWNER
+    @pownerID int
+    
+AS
+BEGIN
+    BEGIN TRY
+    DELETE FROM OWNER
+    WHERE ownerID = @pownerID
+
+    END TRY
+    BEGIN CATCH
+    END CATCH
+END
+GO
